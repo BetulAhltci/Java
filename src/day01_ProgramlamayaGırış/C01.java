14dadb258aa04ced78aa565448afbfc11797e68c
BetulAhlatci <betulahlatci13@gmail.com>
2023-02-21 20:43:45 +0300

    21.02.2023

--- src/day01_ProgramlamayaGırış/C01.java
+++ src/day01_ProgramlamayaGırış/C01.java
@@ -3,6 +3,7 @@ package day01_ProgramlamayaGırış;
 public class C01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
 
     }
 }
